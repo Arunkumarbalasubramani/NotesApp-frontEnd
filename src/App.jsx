e0b937ca56d50a2e29fa988ee957c7e1d79686be
Arunkumarbalasubramani <arunkumar.vbs.b@gmail.com>
2023-11-20 00:12:24 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import "./App.css";
-
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigationbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <h1>Welcome to NotoSphere</h1>
+      <Navigationbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
